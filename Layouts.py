ca0d9ee63ea4b01a3e8f0083c03b51395cfab1dc
peterpanning <peter2mutch@gmail.com>
2018-07-30 21:16:02 -0700

    Updates Layout and Image_Browser classes

--- Layouts.py
+++ Layouts.py
@@ -1,9 +1,10 @@
 from PyQt5.QtWidgets import QHBoxLayout, QGridLayout
+from Image import Image
 
-class Thumbnail_Layout(QHBoxLayout):
+class Thumbnail_layout(QHBoxLayout):
     def __init__(self):
-        super().__init__()
+        super.__init__()
 
-class Zoomed_Layout(QGridLayout):
+class Zoomed_layout(QGridLayout):
     def __init__(self):
-        super().__init__()
+        super.__init__()
